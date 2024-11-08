0cb4b8d4dee9d9643f3bf8d7061f0dfc9eaf775c
xavieryn <61944157+xavieryn@users.noreply.github.com>
2024-11-08 01:16:44 -0500

    Get rid of unused components

--- components/HomePageCard.tsx
+++ components/HomePageCard.tsx
@@ -1,5 +1,6 @@
 import { db } from '@/firebase';
 import { doc } from 'firebase/firestore';
+import Link from 'next/link';
 import React from 'react';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 
@@ -16,14 +17,19 @@ interface HomePageCardProps {
 
 function HomePageCard({ org }: HomePageCardProps) {
     const [data] = useDocumentData(doc(db, "organizations", org.orgId));
+    const basePath = `/org/${org.orgId}`;
 
     return (
-        <div className="group-card flex flex-col shadow-md p-4 mb-4 rounded-lg bg-white dark:bg-gray-800 w-80">
-            <h3 className="text-md font-semibold mb-2 text-gray-900 dark:text-gray-100">
-                {data?.title}
-            </h3>
-            {/* Add more content as needed */}
-        </div>
+        <Link href={basePath}>
+            <div className="group-card flex flex-col shadow-md p-4 mb-4 rounded-lg bg-white dark:bg-gray-800 w-80 h-80">
+                <h1 className="text-md font-semibold mb-2 text-gray-900 dark:text-gray-100">
+                    {data?.title}
+                </h1>
+                <h3>
+                    {data?.admins[0]}
+                </h3>
+            </div>
+        </Link>
     );
 }
 
