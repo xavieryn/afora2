60abf96dbd8a44b714fcddf4f57de7d4feafa277
Welch <weq511762338@gmail.com>
2024-11-20 17:24:33 -0500

    added temp profile pic

--- components/MemberList.tsx
+++ components/MemberList.tsx
@@ -25,9 +25,10 @@ const MemberList = ({ admins, members, userRole }: { admins: string[]; members: 
                 </TableHeader>
                 <TableBody>
                     {admins.map((admin, index) => (
-                        <TableRow key={index}>
-                            <TableCell className="font-medium">{admin}</TableCell>
-                        </TableRow>
+                        <TableCell className="font-medium flex items-center">
+                        <img src="https://static.vecteezy.com/system/resources/previews/024/983/914/non_2x/simple-user-default-icon-free-png.png" alt="default icon" className="w-6 h-6 mr-2 rounded-full" />
+                        {admin}
+                    </TableCell>
                     ))}
                 </TableBody>
             </Table>
@@ -41,7 +42,10 @@ const MemberList = ({ admins, members, userRole }: { admins: string[]; members: 
                 <TableBody>
                     {members.map((member, index) => (
                         <TableRow key={index}>
-                            <TableCell className="font-medium">{member}</TableCell>
+                            <TableCell className="font-medium flex items-center">
+                                <img src="https://static.vecteezy.com/system/resources/previews/024/983/914/non_2x/simple-user-default-icon-free-png.png" alt="default icon" className="w-6 h-6 mr-2 rounded-full" />
+                                {member}
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
